e25600f7f42e8ca07f9f170680920373e80a622e
MSP-Greg <MSP-Greg@users.noreply.github.com>
2016-08-08 13:21:19 -0500

    test Literal Test w/ .freeze

--- test/literal_test.rb
+++ test/literal_test.rb
@@ -1,24 +1,24 @@
 class LiteralTest
 
-  STRINGS_ARRAY_w = %w[I think this is correct Strings Array w]
+  STRINGS_ARRAY_w = %w[I think this is correct Strings Array w].freeze
 
-  STRINGS_ARRAY_W = %W[I think this is correct Strings Array W]
+  STRINGS_ARRAY_W = %W[I think this is correct Strings Array W].freeze
     
-  SYMBOLS_ARRAY_i = %i[I think this is correct Symbols Array i]
+  SYMBOLS_ARRAY_i = %i[I think this is correct Symbols Array i].freeze
 
-  SYMBOLS_ARRAY_I = %I[I think this is correct Symbols Array I]
+  SYMBOLS_ARRAY_I = %I[I think this is correct Symbols Array I].freeze
 
   def init
   
     inter = 'interpolation'
   
-    strings_array_w = %w[I think this is correct Strings Array w]
+    strings_array_w = %w[I think this is correct Strings Array w].freeze
 
-    strings_array_W = %W[I think this is correct Strings Array W #{inter}]
+    strings_array_W = %W[I think this is correct Strings Array W #{inter}].freeze
       
-    symbols_array_i = %i[I think this is correct Symbols Array i]
+    symbols_array_i = %i[I think this is correct Symbols Array i].freeze
 
-    symbols_array_I = %I[I think this is correct Symbols Array I #{inter}]
+    symbols_array_I = %I[I think this is correct Symbols Array I #{inter}].freeze
 
   end
 
