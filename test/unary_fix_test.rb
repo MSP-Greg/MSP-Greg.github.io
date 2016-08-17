7b0146da2ed7c6c8bbcc8e94972671f43886c036
MSP-Greg <MSP-Greg@users.noreply.github.com>
2016-08-16 20:31:40 -0500

    unary_fix_test.rb clean up

--- test/unary_fix_test.rb
+++ test/unary_fix_test.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 # cd /repros/test
+# cd /GitHub/yard/benchmarks
 # ruby unary_fix_test.rb
 
 require 'yard'
@@ -25,7 +26,7 @@ module YARD ; module Parser ; module Ruby
               Array === (opt_node = sub_arg.last.last)
               p_range = p_node.source_range
               opt_range = opt_node.source_range
-              if opt_range.last != p_range.last
+              if opt_range.last != p_range.last && ![:list, :string_content].include?(opt_node.type)
                 sub_arg.last.source_range = (p_range.first..opt_range.last)
               end
             end
@@ -38,8 +39,6 @@ module YARD ; module Parser ; module Ruby
     end
   end
 
-  # below method I added for testing, could be added as a helper function
-  # not used in any of the code for the fix
   class AstNode < Array
     
     # Similar to traverse, but filters by {#type} matching node_type parameter
@@ -59,49 +58,53 @@ end ; end ; end
 module UnaryFix
 
 Base = YARD::Parser::Ruby
-Base::RipperParser.send(:alias_method, :on_params_orig, :on_params)
 Core = RSpec::Core
-Parser = Base::RubyParser
+RParser = Base::RipperParser
+ RParser.send(:alias_method, :on_params_orig, :on_params)
+ RParser.send(:alias_method, :on_params, :t2_on_params)
 
   class << self
 
     def bench
       require 'net/http'
-      # used this file as a quick search found it to have a lot of methods
       file = Net::HTTP.method(:proxy_port).source_location[0]
       const_set(:Net, nil)
 
-      # Or, use RipperParser (ruby_parser.rb)
-      # file = Base::RipperParser.new('', '').method(:on_array).source_location[0]
-      
       src  = File.read(file) 
       times = 50
       orig_file = ''
       fix_file = ''
-      ripper = Base::RipperParser
+      ripper = RParser
       
       orig_meth_orig = nil
       orig_meth_t2   = nil
       fix_meth_orig  = nil
       fix_meth_t2    = nil
+
       puts
-      
       puts "Benchmark - Current vs Unary Fix ----------------------"
       puts "  Parse Ruby Std-Lib lib/net/http.rb"
-      puts
+      puts      
+
       Benchmark.bmbm do |b|
-        ripper.send(:alias_method, :on_params, :on_params_orig)
-        b.report("Current")   { times.times { ripper.new(src, file).parse } }
-        t = ripper.new(src, file)
+        RParser.send(:alias_method, :on_params, :on_params_orig)
+
+        b.report("Current")   { times.times { RParser.new(src, file).parse } }
+
+        # below just to check methods
+        t = RParser.new(src, file)
         orig_meth_orig = (t.method(:on_params) == t.method(:on_params_orig)) ? '==' : '!='
         orig_meth_t2   = (t.method(:on_params) == t.method(:t2_on_params))   ? '==' : '!='
-        
         t = nil
 
-        ripper.send(:alias_method, :on_params, :t2_on_params)
-        fix_file = ripper.new(src, file).method(:on_params).source_location[0]
-        b.report("Unary Fix")  { times.times { ripper.new(src, file).parse } }
-        t = ripper.new(src, file)
+        RParser.send(:alias_method, :on_params, :t2_on_params)
+
+        fix_file = RParser.new(src, file).method(:on_params).source_location[0]
+
+        b.report("Unary Fix")  { times.times { RParser.new(src, file).parse } }
+
+        # below just to check methods
+        t = RParser.new(src, file)
         fix_meth_t2   = (t.method(:on_params) == t.method(:t2_on_params))   ? '==' : '!='
         fix_meth_orig = (t.method(:on_params) == t.method(:on_params_orig)) ? '==' : '!='
         t = nil
@@ -119,10 +122,10 @@ Parser = Base::RubyParser
         src = []
 
         src << ["unary operators with no block parameter",
-          [['a', '-1.0'], ['b', '-1']],
+          [['a', '-1.0'], ['b', '-1'], ['c', '-test']],
           <<-eof
           class Unary
-            def minus(a = -1.0, b = -1)
+            def minus(a = -1.0, b = -1, c = -test)
             end
           end
           eof
@@ -155,15 +158,16 @@ Parser = Base::RubyParser
         
             describe "#{s[2][/def.+?\)/]} - #{s[0]}" do
             
-              def_node = Parser.parse(s[2], 't').ast.jump(:def)
+              def_node = RParser.parse(s[2], 't').ast.jump(:def)
               params = def_node.parameters
-              it "parses correct parameters source" do
+              it "generates correct parameters source for MethodObject#signature" do
                 expect(params.source).to eq("#{s[2][/\(([^\)]+)/,1]}")
               end
               
-              it "YARD::Handlers::Ruby::MethodHandler#format_args generates a " \
-                "correct parameter array" do
-                hndlr = YARD::Handlers::Ruby::MethodHandler.new(Parser, def_node)
+#              it "YARD::Handlers::Ruby::MethodHandler#format_args generates a " \
+#                "correct parameter array for tags" do
+              it "generates a correct array for MethodObject#parameters" do
+                hndlr = YARD::Handlers::Ruby::MethodHandler.new(RParser, def_node)
                 args = hndlr.format_args                
                 expect(args).to eq(s[1])
               end
@@ -175,16 +179,16 @@ Parser = Base::RubyParser
     end
 
     def run_rspec
-      puts "================================ RSpec - YARD Current =================================="
+      puts "================================= RSpec - YARD Current ==================================="
 
-      Base::RipperParser.send(:alias_method, :on_params, :on_params_orig)
+      RParser.send(:alias_method, :on_params, :on_params_orig)
       UnaryFix.rspec_reload
       RSpec::Core::Runner.run(['-c', '-f', 'd'])
 
       puts"\n\n"
-      puts "================================ RSpec - YARD Unary Fix ================================"
+      puts "================================= RSpec - YARD Unary Fix ================================="
 
-      Base::RipperParser.send(:alias_method, :on_params, :t2_on_params)      
+      RParser.send(:alias_method, :on_params, :t2_on_params)      
       RSpec.reset
       UnaryFix.rspec_reload
       RSpec::Core::Runner.run(['-c', '-f', 'd'])
@@ -195,3 +199,4 @@ end
 
 UnaryFix.bench
 UnaryFix.run_rspec
+
